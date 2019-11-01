20bde7efb48f38514e7a27c1433413154e7faab9
henrypadua <henrybpj@live.com>
2019-11-01 11:58:06 -0300

    Ajustando os arquivos do redux com Duck Pattern, e fazendo a função para verificar repositório duplicados

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as FavoriteActions from '../../store/actions/favorites';
+import { Creators as FavoriteActions } from '../../store/ducks/favorites';
 
 class Main extends Component {
   static propTypes = {
@@ -18,7 +18,7 @@ class Main extends Component {
           url: PropTypes.string
         })
       ),
-      error: PropTypes.oneOfType(null, PropTypes.string)
+      error: PropTypes.string
     }).isRequired
   };
 
